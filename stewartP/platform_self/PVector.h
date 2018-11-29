b38f7a3b8dca31635dbc24e7f41ac8cf225aadff
EneloopAA <41654945+EneloopAA@users.noreply.github.com>
2018-11-29 13:56:09 +0900

    Update PVector.h
    added functions

--- stewartP/platform_self/PVector.h
+++ stewartP/platform_self/PVector.h
@@ -1,4 +1,6 @@
 class PVector{
+	
+	
 public:
 	float x;
 	float y;
@@ -10,4 +12,45 @@ public:
 		y=b;
 		z=c;
 	}
+	
+	void set(float a=0,float b=0,float c=0){
+		x=a;
+		y=b;
+		z=c;
+	}
+	
+	void set(PVector a){
+		x=a.x;
+		y=a.y;
+		z=a.z;
+	}
+	
+	void add(float a=0,float b=0,float c=0){
+		x+=a;
+		y+=b;
+		z+=c;
+	}
+	void add(PVector a){
+		x+=a.x;
+		y+=a.y;
+		z+=a.z;
+	}
+	
+	void sub(PVector a){
+		x-=a.x;
+		y-=a.y;
+		z-=a.z;
+	}
+	
+	float magSq(){
+		float ret = 0;
+		ret += x*x;
+		ret += y*y;
+		ret += z*z;
+		return ret;
+	}
+	
 };
+
+
+
